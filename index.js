2dc20d298a58caa0c5e8e637a7f367421aca900c
Robert Chen <robertchen234@gmail.com>
2018-08-17 21:59:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ if (name === "Susan") {
   var name = "Susan";
 }
 
-if (height = "70") {
+if (height === "70") {
   
   console.log("The height is correct")
 } else {
   
-  var height = "70";
+  parseInt height = 70;
 }
 
 
